9f3a88404f3a89273e9ed834e42f66d1b6b387ed
Alexey Chuprov <alexey.chuprov@gmail.com>
2020-01-28 14:24:24 +0300

    Updates

---
+++ precache-manifest.908b26c8df52124c6fe015a762bf7ae4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c99c7e8e2d5ed53f48d6522b1cca51",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "a16107a7699318e375b7",
+    "url": "/my-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "c586403b1f342a4a93ed",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-app/static/js/2.16dedccb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a16107a7699318e375b7",
+    "url": "/my-app/static/js/main.0b4e31ec.chunk.js"
+  },
+  {
+    "revision": "134c6d7b033e4b816dc9",
+    "url": "/my-app/static/js/runtime-main.56c001b7.js"
+  }
+]);
